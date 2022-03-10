1327c0a8567637c0531cc7bb15892e9f4729cff6
Samer A <thedev.samer@gmail.com>
2022-03-10 02:20:31 +0200

    ✔️  Created: layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,114 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { PhotoCamera } from "@material-ui/icons";
+import useStyles from "./styles";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  Button,
+  ButtonGroup,
+} from "@material-ui/core";
 
 function App() {
-  return <div>Hello World!</div>;
+  const classes = useStyles("");
+
+  return (
+    <Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography className={classes.brand} variant="h6">
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+
+            <Typography
+              variant="h6"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello Everyone! This is a photo album, and I'm trying to make this
+              sentence as logn as possible, so we can see how does it look like
+              on the screen.
+            </Typography>
+
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my photos
+                  </Button>
+                </Grid>
+
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            <Grid item>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image="https://source.unsplash.com/random"
+                  title="Image title"
+                />
+
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterButtom variant="h5">
+                    Heading
+                  </Typography>
+
+                  <Typography>
+                    This is a media card. You can use the section to describe
+                    the card.
+                  </Typography>
+                </CardContent>
+
+                <CardActions>
+                  <Button size="sm" color="primary">
+                    View
+                  </Button>
+
+                  <Button size="sm" color="primary">
+                    Edit
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,32 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => {
+  return {
+    brand: {
+      marginLeft: "10px",
+    },
+    container: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(8, 0, 6),
+    },
+    buttons: {
+      marginTop: "40px",
+    },
+    cardGrid: {
+      padding: "20px 0",
+    },
+    card: {
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+    },
+    cardMedia: {
+      paddingTop: "56.25%", // Gives us a 16:9 aspect ratio
+    },
+    cardContent: {
+      flexGrow: 1,
+    },
+  };
+});
+
+export default useStyles;
